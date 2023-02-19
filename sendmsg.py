8a46425e01ada407f07fee15e2e5b907380e1e24
amr-noorwali <113013055+amr-noorwali@users.noreply.github.com>
2023-02-19 17:27:33 +0300

    Add files via upload

---
+++ sendmsg.py
@@ -0,0 +1,43 @@
+import pywhatkit 
+
+
+
+
+number='+966592019911'                         #in variable "number" write the number you want to send message
+                   
+                                                           
+                                                          
+pywhatkit.sendwhatmsg(                         #put the variable, then message, the time in 24 hours and 60 minute   
+
+number,
+"Hello",15,18
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
